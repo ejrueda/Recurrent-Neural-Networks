d00a5dde1959492dd84a1f8033e444117c417b4a
ejrueda <ejrueda95g@gmail.com>
2018-04-29 10:21:21 -0500

    update 4.4

--- code/multi_signal.py
+++ code/multi_signal.py
@@ -1,16 +1,19 @@
-def multi_signal(s_A, s_B):
+def multi_signal(s_A, s_B, new_col):
     """
     dada dos señales s_A y s_B, se obtiene una multiseñal donde la
     señal que predomina para la contrucción es s_A
+    new_col, par darle nombre a la nueva columna
     """
+    import numpy as np
+    import pandas as pd
     #saco los valores que si están en el mismo instante de tiempo
     #se filtra la señal s_B por los indices que tiene s_A
     values = s_B.filter(items=s_A.index, axis=0)
-    #inserto una columa llena de ceros para posteriormente colocar ahi los valores a utilizar
-    s_A.insert(loc=2, column=s_B.columns[0]+'new', value=np.zeros((s_A.shape[0],1)))
+    #inserto una columna llena de ceros para posteriormente colocar ahi los valores a utilizar
+    s_A.insert(loc=2, column=new_col, value=np.zeros((s_A.shape[0],1)))
     #llenamos con los datos
-    s_A.loc[values.index,s_B.columns[0]+'new'] = values.loc[values.index,values.columns[0]]
+    s_A.loc[values.index,new_col] = values.loc[values.index,values.columns[0]]
     #llenamos los ceros
-    s_A.loc[s_A.loc[s_A.index,s_B.columns[0]+'new']==0, s_B.columns[0]+'new'] = s_A.loc[s_A.loc[s_A.index,s_B.columns[0]+'new']==0,s_A.columns[0]]
+    s_A.loc[s_A.loc[s_A.index,new_col]==0, s_B.new_col] = s_A.loc[s_A.loc[s_A.index,new_col]==0,s_A.columns[0]]
     
     return s_A
