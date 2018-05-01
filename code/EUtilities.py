5744d95533164b9d6dd8aa2ba5e977e751ab1111
ejrueda <ejrueda95g@gmail.com>
2018-04-30 19:45:26 -0500

    update 4.5

--- code/EUtilities.py
+++ code/EUtilities.py
@@ -74,7 +74,7 @@ class EUtilities:
     
     #para crear señales multiples
     
-    def multi_signal(self,s_A, s_B):
+    def multi_signal(self,s_A, s_B, new_col, new_loc):
         """
         dada dos señales s_A y s_B, se obtiene una multiseñal donde la
         señal que predomina para la contrucción es s_A
@@ -85,11 +85,11 @@ class EUtilities:
         #se filtra la señal s_B por los indices que tiene s_A
         values = s_B.filter(items=s_A.index, axis=0)
         #inserto una columa llena de ceros para posteriormente colocar ahi los valores a utilizar
-        s_A.insert(loc=2, column=s_B.columns[0]+'new', value=np.zeros((s_A.shape[0],1)))
+        s_A.insert(loc=new_loc, column=new_col, value=np.zeros((s_A.shape[0],1)))
         #llenamos con los datos
-        s_A.loc[values.index,s_B.columns[0]+'new'] = values.loc[values.index,values.columns[0]]
+        s_A.loc[values.index,new_col] = values.loc[values.index,values.columns[0]]
         #llenamos los ceros
-        s_A.loc[s_A.loc[s_A.index,s_B.columns[0]+'new']==0, s_B.columns[0]+'new'] = s_A.loc[s_A.loc[s_A.index,s_B.columns[0]+'new']==0,s_A.columns[0]]
+        s_A.loc[s_A.loc[s_A.index,new_col]==0, new_col] = s_A.loc[s_A.loc[s_A.index,new_col]==0,s_A.columns[0]]
 
         return s_A
 
